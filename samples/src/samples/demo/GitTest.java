c68e40c206c393ca5be8666d8061887051570f61
ztnuyu <izzatisuhaimi@icloud.com>
2021-03-18 14:45:45 +0800

    Initial commit second

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -2,7 +2,8 @@ package samples.demo;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git Is Fun");
+		System.out.println("Git is fun");
+		System.out.println("Git is cool");
 	}
 
 }
