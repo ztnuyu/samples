9927fa8e0078bbbb4ab62c739313397f66cb417c
ztnuyu <izzatisuhaimi@icloud.com>
2021-03-18 14:58:14 +0800

    fifth commit

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,16 +3,6 @@ package samples.demo;
 public class Location {
 	
 	private String locationname;
-	private int poscode;
-	
-	 public int getPoscode() {
-	        return this.poscode;
-	    }
-
-	    public void setPoscode(int posc) {
-	        this.poscode = posc;
-	    }
-	
 	
 	public String getLocation() {
 		return this.locationname;
