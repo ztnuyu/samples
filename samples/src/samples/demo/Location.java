71e6a170fa4aae9fb026fa086bc4587a35a541aa
ztnuyu <izzatisuhaimi@icloud.com>
2021-03-18 14:51:25 +0800

    third commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+	
+	private String locations;
+	
+	public String getLocation() {
+		return this.locations;
+	}
+	
+	public void setLocation (String loc) {
+		this.locations = loc;
+	}
+	
+}
